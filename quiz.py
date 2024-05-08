d6782dd961444c2760ee77896eee27065d055a74
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-05-08 02:46:56 +0100

    add questions

--- quiz.py
+++ quiz.py
@@ -5,8 +5,31 @@ def main():
 
     if playing != 'yes':
         quit()
-
     print("Okay! Let's play :)")
+
+    answer = input("What is the world's oldest football competition? ").lower()
+    if answer == "fa Cup":
+        print("Correct")
+    else:
+        print("Incorrect")
+
+    answer = input("Which player is allowed to use hands? ").lower()
+    if answer == "goalkeeper":
+        print("Correct")
+    else:
+        print("Incorrect")
+    
+    answer = input("What is football's highest governing body? ").lower()
+    if answer == "fifa":
+        print("Correct")
+    else:
+        print("Incorrect")
+    
+    answer = input("How many minutes constitute a half? ").lower()
+    if answer == ("45", "45mins", '45 minutes'):
+        print("Correct")
+    else:
+        print("Incorrect")
 
 if __name__ == "__main__":
     main()
