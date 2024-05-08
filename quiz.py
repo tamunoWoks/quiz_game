2ed10315b20833733ad9f6e37761b27f61f91984
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-05-08 03:04:18 +0100

    Include player score

--- quiz.py
+++ quiz.py
@@ -1,35 +1,43 @@
 def main():
     print('Welcome to my Soccer Quiz!')
 
-    playing = input('Do you want to play? ').lower()
+    playing = input('Do you want to play? ')
 
-    if playing != 'yes':
+    if playing.lower() != 'yes':
         quit()
     print("Okay! Let's play :)")
+    score = 0
 
     answer = input("What is the world's oldest football competition? ").lower()
-    if answer == "fa Cup":
+    if answer == "fa cup":
         print("Correct")
+        score +=1
     else:
         print("Incorrect")
 
     answer = input("Which player is allowed to use hands? ").lower()
     if answer == "goalkeeper":
         print("Correct")
+        score +=1
     else:
         print("Incorrect")
     
     answer = input("What is football's highest governing body? ").lower()
     if answer == "fifa":
         print("Correct")
+        score +=1
     else:
         print("Incorrect")
     
     answer = input("How many minutes constitute a half? ").lower()
-    if answer == ("45", "45mins", '45 minutes'):
-        print("Co"45"rrect")
+    if "45" in answer:
+        print("Correct")
+        score +=1
     else:
         print("Incorrect")
+
+    print(f"You got {score} correct")
+    print(f"You scored {round((score/4) * 100)}%")
 
 if __name__ == "__main__":
     main()
